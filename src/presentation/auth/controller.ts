71349670f9a1f9a1fa9eaee6409d48b2e37a84a0
Leon Casas <leonn.casass@gmail.com>
2024-06-14 16:46:23 -0600

    - DTO and error handling were implemented

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
+import { RegisterUserDTO } from "../../domain";
 
 export class AuthController {
   constructor() {}
 
   registerUser = (req: Request, res: Response) => {
-    res.json("registerUser Controller");
+    const [error, registerUserDto] = RegisterUserDTO.create(req.body);
+    if (error) return res.status(400).json({ error });
+
+    res.json(req.body);
   };
 
   loginUser = (req: Request, res: Response) => {
