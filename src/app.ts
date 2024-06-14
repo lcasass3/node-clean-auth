202dfded242d141b492f133842c7a5488e89efb9
Leon Casas <leonn.casass@gmail.com>
2024-06-14 00:02:23 -0600

    - Created App router and implemented it on th server definition and use

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from "./config";
+import { AppRoutes } from "./presentation/routes";
 import { Server } from "./presentation/server";
 
 (() => {
@@ -8,5 +9,8 @@ import { Server } from "./presentation/server";
 async function main() {
   //TODO: await db
 
-  new Server({ port: envs.PORT }).start();
+  new Server({
+    port: envs.PORT,
+    routes: AppRoutes.routes,
+  }).start();
 }
