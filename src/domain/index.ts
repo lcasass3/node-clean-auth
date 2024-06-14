3c448c6169561258fd9f8b03d85c801343ed7d24
Leon Casas <leonn.casass@gmail.com>
2024-06-14 17:14:50 -0600

    - Created auth datasource implementation

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,4 +1,5 @@
 export * from "./dtos/auth/register-user.dto";
 export * from "./entities/user.entity";
+export * from "./datasources/auth.datasource";
 export * from "./repositories/auth.respository";
 export * from "./errors/custom.error";
---
+++ src/infrastructure/datasources/auth.datasource.impl.ts
@@ -0,0 +1,21 @@
+import {
+  AuthDatasource,
+  CustomError,
+  RegisterUserDTO,
+  User,
+} from "../../domain";
+
+export class AuthDataSourceImpl implements AuthDatasource {
+  async register(registerUserDto: RegisterUserDTO): Promise<User> {
+    const { name, email, password } = registerUserDto;
+
+    try {
+      // TODO: Add database logic to create user here
+
+      return new User("1", name, email, password, "ADMIN");
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+    }
+    throw CustomError.internalServer();
+  }
+}
