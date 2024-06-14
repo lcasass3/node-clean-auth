a92d5d8871fa0c2cb87778dda68bac9ab065533a
Leon Casas <leonn.casass@gmail.com>
2024-06-14 16:34:14 -0600

    - RegisterUserDTO with create method was created

---
+++ src/domain/dtos/auth/register-user.dto.ts
@@ -0,0 +1,21 @@
+import { Validators } from "../../../config";
+
+export class RegisterUserDTO {
+  private constructor(
+    public name: string,
+    public email: string,
+    public password: string
+  ) {}
+
+  static create(object: { [key: string]: any }): [string?, RegisterUserDTO?] {
+    const { name, email, password } = object;
+
+    if (!name) return ["Missing name"];
+    if (!email) return ["Missing email"];
+    if (!Validators.email.test(email)) return ["email not valid"];
+    if (!password) return ["Missing password"];
+    if (password.length < 6) return ["Password too short"];
+
+    return [undefined, new RegisterUserDTO(name, email.toLowerCase, password)];
+  }
+}
